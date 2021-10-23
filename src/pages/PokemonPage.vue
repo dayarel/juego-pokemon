1e2b3c2fbf6c3cfdf66f01cf7c72640cec4bc670
DESKTOP-UGPE3J9\nerad <dcortes@evolmind.com>
2021-10-23 18:59:57 +0200

    Muestra nombres de pokemons

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,17 +1,41 @@
 <template>
-  <h1>¿Quién es éste Pokémon</h1>
+  <h1 v-if="!pokemon">Espere por favor...</h1>
+  <div v-else>
+    <h1>¿Quién es éste Pokémon</h1>
 
-  <!-- TODO Imagen -->
-  <PokemonPicture :pokemonId="15" :showPokemon="false" />
+    <!-- TODO Imagen -->
+    <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
 
-  <!-- TODO Opciones -->
-  <PokemonOptions />
+    <!-- TODO Opciones -->
+    <PokemonOptions :pokemons="pokemonArr" />
+  </div>
 </template>
 
 <script>
 import PokemonOptions from "@/components/PokemonOptions";
 import PokemonPicture from "@/components/PokemonPicture";
+
+import getPokemonOptions from "@/helpers/getPokemonOptions";
+
 export default {
   components: { PokemonOptions, PokemonPicture },
+  data() {
+    return {
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false,
+    };
+  },
+  methods: {
+    async mixPokemonArray() {
+      this.pokemonArr = await getPokemonOptions();
+
+      const rndInt = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemonArr[rndInt];
+    },
+  },
+  mounted() {
+    this.mixPokemonArray();
+  },
 };
 </script>
